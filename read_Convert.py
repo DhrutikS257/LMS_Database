b7af25da93473f7520304b656aa5c0dd84b1f50a
Dhrutik-S <solankidhrutik0602@gmail.com>
2023-04-15 09:11:12 -0500

    converted some csv files

--- read_Convert.py
+++ read_Convert.py
@@ -1,3 +1,6 @@
 import pandas as pd
+import sqlite3
 
-readFile = pd.read_excel()
+readFile = pd.read_excel('C:\\Users\\Dhrutik Solanki\\Documents\\GitHub\\LMS_Database\\Data\\Book_Loans.xlsx')
+
+readFile.to_csv('C:\\Users\\Dhrutik Solanki\\Documents\\GitHub\\LMS_Database\\CSVData\\Book_Loans.csv',index=False)
