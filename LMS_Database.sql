0f111e334662ff768df5ebf3909fe185e4765f29
Dhrutik <solankidhrutik0602@gmail.com>
2023-04-16 19:59:48 -0500

    completed q10

--- LMS_Database.sql
+++ LMS_Database.sql
@@ -24,6 +24,29 @@
 -- WHERE bl.date_out BETWEEN '2022-03-05' AND '2022-03-23';
 
 -- SELECT bo.name, bo.card_no
--- FROM BORROWER as bo
+-- FROM BOOK_LOANS AS bl
+-- JOIN  BORROWER as bo ON bl.card_no = bo.card_no
+-- WHERE bl.returned_date IS NULL
+-- GROUP BY bo.card_no;
+
+-- SELECT lb.branch_id,lb.branch_name, 
+--         SUM(CASE WHEN bl.returned_date <= bl.due_date THEN 1 ELSE 0 END) AS Returned,
+--         SUM(CASE WHEN bl.returned_date IS NULL THEN 1 ELSE 0 END) AS Borrowed,
+--         SUM(CASE WHEN bl.returned_date > bl.due_date THEN 1 ELSE 0 END) AS Late
+-- FROM BOOK_LOANS AS bl 
+-- JOIN LIBRARY_BRANCH AS lb ON bl.branch_id = lb.branch_id
+-- JOIN BOOK AS b ON b.book_id = bl.book_id
+-- GROUP BY lb.branch_id, lb.branch_name;
+
+-- SELECT MAX(julianday(returned_date) - julianday(date_out)) AS Max_Borrowed_Days
+-- FROM BOOK_LOANS;
+
+-- SELECT bo.name, bo.address
+-- FROM BORROWER AS bo
 -- JOIN BOOK_LOANS AS bl ON bl.card_no = bo.card_no
--- WHERE bl.due_date < bl.returned_date;
+-- GROUP bo.name;
+
+SELECT bo.name, bo.address
+FROM BOOK_LOANS AS bl
+JOIN  BORROWER as bo ON bl.card_no = bo.card_no
+GROUP BY bo.card_no;
--- read_Convert.py
+++ read_Convert.py
@@ -5,29 +5,29 @@ import sqlite3
 import csv
 
 
-# # converting .xlsx to .csv
-# curr_path = os.getcwd()
-# data_curr_path = os.path.join(curr_path,'Data')
-# csv_curr_path = os.path.join(curr_path,'CSVData')
+# converting .xlsx to .csv
+curr_path = os.getcwd()
+data_curr_path = os.path.join(curr_path,'Data')
+csv_curr_path = os.path.join(curr_path,'CSVData')
 
-# readFile = pd.read_excel(os.path.join(data_curr_path,'Book_Loans.xlsx'))
+readFile = pd.read_excel(os.path.join(data_curr_path,'Book_Loans.xlsx'))
 
-# readFile.to_csv(os.path.join(csv_curr_path,'Book_Loans.csv'),index=False)
+readFile.to_csv(os.path.join(csv_curr_path,'Book_Loans.csv'),index=False)
 
 
 # # inserting the data into the database
-# curr_path = os.getcwd()
-# curr_data_path = os.path.join(curr_path,'CSVData')
+curr_path = os.getcwd()
+curr_data_path = os.path.join(curr_path,'CSVData')
 
-# with open(os.path.join(curr_data_path,'Book_Loans.csv'),'r') as f:
-#     readFile = csv.reader(f)
+with open(os.path.join(curr_data_path,'Book_Loans.csv'),'r') as f:
+    readFile = csv.reader(f)
 
-#     connDB = sqlite3.connect(os.path.join(curr_path,'Database.db'))
-#     connectDB = connDB.cursor()
+    connDB = sqlite3.connect(os.path.join(curr_path,'Database.db'))
+    connectDB = connDB.cursor()
 
-#     for row in readFile:
-#         connectDB.execute('INSERT INTO BOOK_LOANS (book_id, branch_id, card_no, date_out, due_date, returned_date) VALUES (?, ?, ?, ?, ?, ?)'
-#                         ,(row[0],row[1],row[2],row[3],row[4],row[5]))
+    for row in readFile:
+        connectDB.execute('INSERT INTO BOOK_LOANS (book_id, branch_id, card_no, date_out, due_date, returned_date) VALUES (?, ?, ?, ?, ?, ?)'
+                        ,(row[0],row[1],row[2],row[3],row[4],row[5]))
 
-# connDB.commit()
-# connDB.close()
+connDB.commit()
+connDB.close()
