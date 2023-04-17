b25fed8e34d90a13d12789c167866d4023280f52
Dhrutik-S <solankidhrutik0602@gmail.com>
2023-04-17 17:06:44 -0500

    DONE

--- insert_Data.py
+++ insert_Data.py
@@ -5,15 +5,16 @@ import sqlite3, csv, os
 curr_path = os.getcwd()
 curr_data_path = os.path.join(curr_path,'CSVData')
 
-with open(os.path.join(curr_data_path,'Publisher.csv'),'r') as f:
+#change the csv file name depending on which file you want to enter
+with open(os.path.join(curr_data_path,'Book_Authors.csv'),'r') as f:
     readFile = csv.reader(f)
 
     connDB = sqlite3.connect(os.path.join(curr_path,'Database.db'))
     connectDB = connDB.cursor()
 
     for row in readFile:
-        connectDB.execute('INSERT INTO PUBLISHER (publisher_name, phone, address) VALUES (?, ?, ?)'
-                        ,(row[0],row[1],row[2]))
+        #change the table name, fields, VALUES, and rows depending on the table you want to enter
+        connectDB.execute('INSERT INTO BOOK_AUTHORS (book_id, author_name) VALUES (?, ?)',(row[0],row[1]))
 
 connDB.commit()
 connDB.close()
